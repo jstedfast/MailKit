61bc93910d0fd8f5a567867a850c620bc6a7dcd9
Jeffrey Stedfast <jeff@xamarin.com>
2014-02-06 08:43:34 -0500

    Added unit tests for Exchange Pop3

--- UnitTests/Net/Pop3/Pop3ClientTests.cs
+++ UnitTests/Net/Pop3/Pop3ClientTests.cs
@@ -39,10 +39,12 @@ namespace UnitTests.Net.Pop3 {
 	[TestFixture]
 	public class Pop3ClientTests
 	{
-		readonly Pop3Capabilities Capa1 = Pop3Capabilities.Expire | Pop3Capabilities.StartTLS |
+		readonly Pop3Capabilities ComcastCapa1 = Pop3Capabilities.Expire | Pop3Capabilities.StartTLS |
 			Pop3Capabilities.Top | Pop3Capabilities.UIDL | Pop3Capabilities.User;
-		readonly Pop3Capabilities Capa2 = Pop3Capabilities.Expire | Pop3Capabilities.StartTLS |
+		readonly Pop3Capabilities ComcastCapa2 = Pop3Capabilities.Expire | Pop3Capabilities.StartTLS |
 			Pop3Capabilities.Sasl | Pop3Capabilities.Top | Pop3Capabilities.UIDL | Pop3Capabilities.User;
+		readonly Pop3Capabilities ExchangeCapa = Pop3Capabilities.Sasl | Pop3Capabilities.Top |
+			Pop3Capabilities.UIDL | Pop3Capabilities.User;
 
 		[Test]
 		public void TestBasicPop3Client ()
@@ -66,7 +68,7 @@ namespace UnitTests.Net.Pop3 {
 
 				Assert.IsTrue (client.IsConnected, "Client failed to connect.");
 
-				Assert.AreEqual (Capa1, client.Capabilities);
+				Assert.AreEqual (ComcastCapa1, client.Capabilities);
 				Assert.AreEqual (0, client.AuthenticationMechanisms.Count);
 				Assert.AreEqual (31, client.ExpirePolicy);
 
@@ -77,7 +79,7 @@ namespace UnitTests.Net.Pop3 {
 					Assert.Fail ("Did not expect an exception in Authenticate: {0}", ex);
 				}
 
-				Assert.AreEqual (Capa2, client.Capabilities);
+				Assert.AreEqual (ComcastCapa2, client.Capabilities);
 				Assert.AreEqual ("ZimbraInc", client.Implementation);
 				Assert.AreEqual (2, client.AuthenticationMechanisms.Count);
 				Assert.IsTrue (client.AuthenticationMechanisms.Contains ("PLAIN"), "Expected SASL PLAIN auth mechanism");
@@ -130,7 +132,7 @@ namespace UnitTests.Net.Pop3 {
 
 				Assert.IsTrue (client.IsConnected, "Client failed to connect.");
 
-				Assert.AreEqual (Capa1, client.Capabilities);
+				Assert.AreEqual (ComcastCapa1, client.Capabilities);
 				Assert.AreEqual (0, client.AuthenticationMechanisms.Count);
 				Assert.AreEqual (31, client.ExpirePolicy);
 
@@ -141,7 +143,7 @@ namespace UnitTests.Net.Pop3 {
 					Assert.Fail ("Did not expect an exception in Authenticate: {0}", ex);
 				}
 
-				Assert.AreEqual (Capa2, client.Capabilities);
+				Assert.AreEqual (ComcastCapa2, client.Capabilities);
 				Assert.AreEqual ("ZimbraInc", client.Implementation);
 				Assert.AreEqual (2, client.AuthenticationMechanisms.Count);
 				Assert.IsTrue (client.AuthenticationMechanisms.Contains ("PLAIN"), "Expected SASL PLAIN auth mechanism");
@@ -191,7 +193,7 @@ namespace UnitTests.Net.Pop3 {
 
 				Assert.IsTrue (client.IsConnected, "Client failed to connect.");
 
-				Assert.AreEqual (Capa1, client.Capabilities);
+				Assert.AreEqual (ComcastCapa1, client.Capabilities);
 				Assert.AreEqual (0, client.AuthenticationMechanisms.Count);
 				Assert.AreEqual (31, client.ExpirePolicy);
 
@@ -316,6 +318,79 @@ namespace UnitTests.Net.Pop3 {
 				}
 
 				Assert.IsTrue (client.IsConnected, "UnauthorizedAccessException should not cause a disconnect.");
+
+				try {
+					client.Disconnect (true, CancellationToken.None);
+				} catch (Exception ex) {
+					Assert.Fail ("Did not expect an exception in Disconnect: {0}", ex);
+				}
+
+				Assert.IsFalse (client.IsConnected, "Failed to disconnect");
+			}
+		}
+
+		[Test]
+		public void TestExchangePop3Client ()
+		{
+			var commands = new List<Pop3ReplayCommand> ();
+			commands.Add (new Pop3ReplayCommand ("", "exchange.greeting.txt"));
+			commands.Add (new Pop3ReplayCommand ("CAPA\r\n", "exchange.capa.txt"));
+			commands.Add (new Pop3ReplayCommand ("AUTH PLAIN\r\n", "exchange.plus.txt"));
+			commands.Add (new Pop3ReplayCommand ("AHVzZXJuYW1lAHBhc3N3b3Jk\r\n", "exchange.auth.txt"));
+			commands.Add (new Pop3ReplayCommand ("CAPA\r\n", "exchange.capa.txt"));
+			commands.Add (new Pop3ReplayCommand ("STAT\r\n", "exchange.stat.txt"));
+			commands.Add (new Pop3ReplayCommand ("UIDL\r\n", "exchange.uidl.txt"));
+			commands.Add (new Pop3ReplayCommand ("RETR 1\r\n", "exchange.retr1.txt"));
+			commands.Add (new Pop3ReplayCommand ("QUIT\r\n", "exchange.quit.txt"));
+
+			using (var client = new Pop3Client ()) {
+				try {
+					client.ReplayConnect ("localhost", new Pop3ReplayStream (commands, false), CancellationToken.None);
+				} catch (Exception ex) {
+					Assert.Fail ("Did not expect an exception in Connect: {0}", ex);
+				}
+
+				Assert.IsTrue (client.IsConnected, "Client failed to connect.");
+
+				Assert.AreEqual (ExchangeCapa, client.Capabilities);
+				Assert.AreEqual (3, client.AuthenticationMechanisms.Count);
+				Assert.IsTrue (client.AuthenticationMechanisms.Contains ("GSSAPI"), "Expected SASL GSSAPI auth mechanism");
+				Assert.IsTrue (client.AuthenticationMechanisms.Contains ("NTLM"), "Expected SASL NTLM auth mechanism");
+				Assert.IsTrue (client.AuthenticationMechanisms.Contains ("PLAIN"), "Expected SASL PLAIN auth mechanism");
+
+				try {
+					var credentials = new NetworkCredential ("username", "password");
+					client.Authenticate (credentials, CancellationToken.None);
+				} catch (Exception ex) {
+					Assert.Fail ("Did not expect an exception in Authenticate: {0}", ex);
+				}
+
+				Assert.AreEqual (ExchangeCapa, client.Capabilities);
+				Assert.AreEqual (3, client.AuthenticationMechanisms.Count);
+				Assert.IsTrue (client.AuthenticationMechanisms.Contains ("GSSAPI"), "Expected SASL GSSAPI auth mechanism");
+				Assert.IsTrue (client.AuthenticationMechanisms.Contains ("NTLM"), "Expected SASL NTLM auth mechanism");
+				Assert.IsTrue (client.AuthenticationMechanisms.Contains ("PLAIN"), "Expected SASL PLAIN auth mechanism");
+
+				try {
+					var count = client.GetMessageCount (CancellationToken.None);
+					Assert.AreEqual (7, count, "Expected 7 messages");
+				} catch (Exception ex) {
+					Assert.Fail ("Did not expect an exception in GetMessageCount: {0}", ex);
+				}
+
+				try {
+					var uids = client.GetMessageUids (CancellationToken.None);
+					Assert.AreEqual (7, uids.Length, "Expected 7 uids");
+				} catch (Exception ex) {
+					Assert.Fail ("Did not expect an exception in GetMessageUids: {0}", ex);
+				}
+
+				try {
+					var message = client.GetMessage (0, CancellationToken.None);
+					// TODO: assert that the message is byte-identical to what we expect
+				} catch (Exception ex) {
+					Assert.Fail ("Did not expect an exception in GetMessage: {0}", ex);
+				}
 
 				try {
 					client.Disconnect (true, CancellationToken.None);
