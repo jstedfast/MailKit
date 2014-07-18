7d4678dcd16edf12bb1fb7efd955acd6c0d33d04
Jeffrey Stedfast <fejj@gnome.org>
2014-07-17 21:34:50 -0400

    Added missing file for the ImapClientDemo sample

---
+++ samples/ImapClientDemo/ImapClientDemo/MessageSelectedEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MailKit;
+using MimeKit;
+
+namespace ImapClientDemo
+{
+	class MessageSelectedEventArgs : EventArgs
+	{
+		public MessageSelectedEventArgs (IMailFolder folder, UniqueId uid, BodyPart body)
+		{
+			Folder = folder;
+			UniqueId = uid;
+			Body = body;
+		}
+
+		public IMailFolder Folder {
+			get; private set;
+		}
+
+		public UniqueId UniqueId {
+			get; private set;
+		}
+
+		public BodyPart Body {
+			get; private set;
+		}
+	}
+}
