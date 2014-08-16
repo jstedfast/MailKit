8c746227387b15ab3df33c481b8015e7ee1939a7
Jeffrey Stedfast <jeff@xamarin.com>
2014-08-15 20:22:44 -0400

    Bumped version to 0.94.0.0

--- MailKit/Properties/AssemblyInfo.cs
+++ MailKit/Properties/AssemblyInfo.cs
@@ -79,6 +79,6 @@ using System.Runtime.InteropServices;
 //
 // If there have only been bug fixes, bump the Micro Version and/or the Build Number
 //    in the AssemblyFileVersion attribute.
-[assembly: AssemblyInformationalVersion ("0.93.0.0")]
-[assembly: AssemblyFileVersion ("0.93.0.0")]
-[assembly: AssemblyVersion ("0.93.0.0")]
+[assembly: AssemblyInformationalVersion ("0.94.0.0")]
+[assembly: AssemblyFileVersion ("0.94.0.0")]
+[assembly: AssemblyVersion ("0.94.0.0")]
