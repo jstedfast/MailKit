7011f0f09ccd6da06ac4c5e6b7c706a11556cbbc
Jeffrey Stedfast <jeff@xamarin.com>
2014-12-15 08:17:01 -0500

    Added Pop3Client.Connect() which takes a Socket argument
    Implements feature needed in issue #128

--- MailKit/Net/Pop3/Pop3Client.cs
+++ MailKit/Net/Pop3/Pop3Client.cs
@@ -536,7 +536,7 @@ namespace MailKit.Net.Pop3 {
 		/// Establishes a connection to the specified POP3 server.
 		/// </summary>
 		/// <remarks>
-		/// <para>Establishes a connection to an POP3 or POP3/S server. If the schema
+		/// <para>Establishes a connection to a POP3 or POP3/S server. If the schema
 		/// in the uri is "pop", a clear-text connection is made and defaults to using
 		/// port 110 if no port is specified in the URI. However, if the schema in the
 		/// uri is "pops", an SSL connection is made using the
@@ -683,6 +683,119 @@ namespace MailKit.Net.Pop3 {
 			engine.Disconnected += OnEngineDisconnected;
 			OnConnected ();
 		}
+
+#if !NETFX_CORE
+		/// <summary>
+		/// Establishes a connection to the specified POP3 server using the provided socket.
+		/// </summary>
+		/// <remarks>
+		/// <para>Establishes a connection to a POP3 or POP3/S server. If the schema
+		/// in the uri is "pop", a clear-text connection is assumed. However, if the
+		/// schema in the uri is "pops", an SSL connection is made using the
+		/// <see cref="MailService.ClientCertificates"/>.</para>
+		/// <para>It should be noted that when using a clear-text POP3 connection,
+		/// if the server advertizes support for the STLS extension, the client
+		/// will automatically switch into TLS mode before authenticating unless
+		/// the <paramref name="uri"/> contains a query string ("?starttls=false")
+		/// to disable it.</para>
+		/// If a successful connection is made, the <see cref="AuthenticationMechanisms"/>
+		/// and <see cref="Capabilities"/> properties will be populated.
+		/// </remarks>
+		/// <param name="uri">The server URI. The <see cref="System.Uri.Scheme"/> should either
+		/// be "pop" to make a clear-text connection or "pops" to make an SSL connection.</param>
+		/// <param name="socket">The socket to use for the connection.</param>
+		/// <param name="cancellationToken">The cancellation token.</param>
+		/// <exception cref="System.ArgumentNullException">
+		/// <para><paramref name="uri"/> is <c>null</c>.</para>
+		/// <para>-or-</para>
+		/// <para><paramref name="socket"/> is <c>null</c>.</para>
+		/// </exception>
+		/// <exception cref="System.ArgumentException">
+		/// <para><paramref name="uri"/> is not an absolute URI.</para>
+		/// <para>-or-</para>
+		/// <para><paramref name="socket"/> is not connected.</para>
+		/// </exception>
+		/// <exception cref="System.ObjectDisposedException">
+		/// The <see cref="Pop3Client"/> has been disposed.
+		/// </exception>
+		/// <exception cref="System.InvalidOperationException">
+		/// The <see cref="Pop3Client"/> is already connected.
+		/// </exception>
+		/// <exception cref="System.OperationCanceledException">
+		/// The operation was canceled via the cancellation token.
+		/// </exception>
+		/// <exception cref="System.IO.IOException">
+		/// An I/O error occurred.
+		/// </exception>
+		/// <exception cref="Pop3CommandException">
+		/// A POP3 command failed.
+		/// </exception>
+		/// <exception cref="Pop3ProtocolException">
+		/// A POP3 protocol error occurred.
+		/// </exception>
+		public void Connect (Uri uri, Socket socket, CancellationToken cancellationToken = default (CancellationToken))
+		{
+			if (uri == null)
+				throw new ArgumentNullException ("uri");
+
+			if (!uri.IsAbsoluteUri)
+				throw new ArgumentException ("The uri must be absolute.", "uri");
+
+			if (socket == null)
+				throw new ArgumentNullException ("socket");
+
+			if (!socket.Connected)
+				throw new ArgumentException ("The socket is not connected.", "socket");
+
+			CheckDisposed ();
+
+			if (IsConnected)
+				throw new InvalidOperationException ("The Pop3Client is already connected.");
+
+			var scheme = uri.Scheme.ToLowerInvariant ();
+			var pops = scheme == "pops" || scheme == "pop3s";
+			var query = uri.ParsedQuery ();
+			Stream stream;
+			string value;
+
+			var starttls = !pops && (!query.TryGetValue ("starttls", out value) || Convert.ToBoolean (value));
+
+			if (pops) {
+				var ssl = new SslStream (new NetworkStream (socket, true), false, ValidateRemoteCertificate);
+				ssl.AuthenticateAsClient (uri.Host, ClientCertificates, DefaultSslProtocols, true);
+				stream = ssl;
+			} else {
+				stream = new NetworkStream (socket, true);
+			}
+
+			probed = ProbedCapabilities.None;
+			if (stream.CanTimeout) {
+				stream.WriteTimeout = timeout;
+				stream.ReadTimeout = timeout;
+			}
+
+			host = uri.Host;
+
+			logger.LogConnect (uri);
+
+			engine.Connect (new Pop3Stream (stream, socket, logger), cancellationToken);
+			engine.QueryCapabilities (cancellationToken);
+
+			if (starttls && (engine.Capabilities & Pop3Capabilities.StartTLS) != 0) {
+				SendCommand (cancellationToken, "STLS");
+
+				var tls = new SslStream (stream, false, ValidateRemoteCertificate);
+				tls.AuthenticateAsClient (uri.Host, ClientCertificates, DefaultSslProtocols, true);
+				engine.Stream.Stream = tls;
+
+				// re-issue a CAPA command
+				engine.QueryCapabilities (cancellationToken);
+			}
+
+			engine.Disconnected += OnEngineDisconnected;
+			OnConnected ();
+		}
+#endif
 
 		/// <summary>
 		/// Disconnect the service.
