104f152cb88ad24c751931c78000153486b5ef5c
Redth <jondick@gmail.com>
2015-02-05 15:13:53 -0500

    Added iOS Imap Demo

---
+++ samples/ImapClientDemo.iOS/ImapClientDemo.iOS/FoldersViewController.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Dialog;
+
+using Foundation;
+using UIKit;
+using MailKit;
+using System.Threading.Tasks;
+
+namespace ImapClientDemo.iOS
+{
+    public partial class FoldersViewController : DialogViewController
+    {
+        MessagesViewController messagesViewController;
+
+        public FoldersViewController () : base (UITableViewStyle.Grouped, null, true)
+        {
+            Root = new RootElement ("Folders");
+        }
+
+        public async override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+
+            var foldersSection = new Section ();
+
+            var personal = Mail.Client.GetFolder (Mail.Client.PersonalNamespaces [0]); 
+
+            // Recursively load all folders and subfolders
+            await LoadChildFolders (foldersSection, personal);
+
+            Root.Clear ();
+            Root.Add (foldersSection);
+        }
+
+        // Recursive function to load all folders and their subfolders
+        async Task LoadChildFolders (Section foldersSection, IMailFolder imapFolder)
+        {
+            foldersSection.Add (new StyledStringElement (imapFolder.FullName, () =>
+                OpenFolder (imapFolder)));
+
+            var subfolders = await imapFolder.GetSubfoldersAsync ();
+
+            foreach (var sf in subfolders)
+                await LoadChildFolders (foldersSection, sf);
+        }
+
+        void OpenFolder (IMailFolder folder)
+        {
+            messagesViewController = new MessagesViewController (folder);
+            NavigationController.PushViewController (messagesViewController, true);
+        }
+    }
+}
---
+++ samples/ImapClientDemo.iOS/ImapClientDemo.iOS/LoginViewController.cs
@@ -0,0 +1,78 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Dialog;
+
+using Foundation;
+using UIKit;
+
+namespace ImapClientDemo.iOS
+{
+    public partial class LoginViewController : DialogViewController
+    {
+        FoldersViewController foldersViewController;
+
+        public LoginViewController () : base (UITableViewStyle.Grouped, null)
+        {
+            Root = new RootElement ("IMAP Login") {
+                new Section ("Server") {
+                    new EntryElement ("Host", "imap.gmail.com", ""),
+                    new EntryElement ("Port", "993", "") {
+                        KeyboardType = UIKeyboardType.NumberPad
+                    },
+                    new CheckboxElement ("Use SSL", false)
+                },
+                new Section ("Account") {
+                    new EntryElement ("Username", "Email / Username", ""),
+                    new EntryElement ("Password", "password", "", true)
+                },
+                new Section () {
+                    new StyledStringElement ("Login", Login)
+                }
+            };
+        }
+
+        async void Login ()
+        {
+            var host = (Root [0] [0] as EntryElement);
+            var port = (Root [0] [1] as EntryElement);
+            var ssl = (Root [0] [2] as CheckboxElement);
+            var username = (Root [1] [0] as EntryElement);
+            var password = (Root [1] [1] as EntryElement);
+
+            host.FetchValue ();
+            port.FetchValue ();
+            username.FetchValue ();
+            password.FetchValue ();
+
+            int iport = 0;
+            int.TryParse (port.Value, out iport);
+
+            try {
+                // Connect to server
+                await Mail.Client.ConnectAsync (host.Value, iport, ssl.Value);
+
+                // Remove this auth mechanism since we don't have an oauth token
+                Mail.Client.AuthenticationMechanisms.Remove ("XOAUTH");
+
+                try {
+                    // Authenticate now that we're connected
+                    await Mail.Client.AuthenticateAsync (username.Value, password.Value);
+
+                    // Show the folders view controller
+                    foldersViewController = new FoldersViewController();
+                    NavigationController.PushViewController (foldersViewController, true);
+
+                } catch (Exception aex) {
+                    Console.WriteLine (aex);
+                    Mail.MessageBox ("Authentication Error", "Failed to Authenticate to server");
+                }
+            } catch (Exception cex) {
+                Console.WriteLine (cex);
+                Mail.MessageBox ("Connection Error", "Failed to connect to server");
+            }
+        }
+    }
+}
