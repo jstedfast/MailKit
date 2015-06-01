37ca44770fdfa84ba9f89b473234bb6c8f029a7c
Jeffrey Stedfast <jeff@xamarin.com>
2015-05-31 21:38:34 -0400

    Fixed cov-build.bat

--- cov-build.bat
+++ cov-build.bat
@@ -1,1 +1,1 @@
-..\cov-analysis\bin\cov-build.exe --dir cov-int "%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /t:Rebuild MimeKit.Net40.sln
+..\cov-analysis\bin\cov-build.exe --dir cov-int "%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /t:Rebuild MailKit.Net40.sln
