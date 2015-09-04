cc9429d3d06e2ab2b2ab48332f3f2285f6699a07
Jeffrey Stedfast <jeff@xamarin.com>
2015-09-04 10:07:45 -0400

    Allow the UIDVALIDITY argument to the COPYUID resp-code to be 0
    A UIDVALIDITY value of 0 is illegal, but this patch allows clients to
    continue interoperating with the broken server.
    
    Fixes issue #240

--- MailKit/Net/Imap/ImapEngine.cs
+++ MailKit/Net/Imap/ImapEngine.cs
@@ -1250,7 +1250,7 @@ namespace MailKit.Net.Imap {
 			case ImapResponseCodeType.CopyUid:
 				var copy = (CopyUidResponseCode) code;
 
-				if (token.Type != ImapTokenType.Atom || !uint.TryParse ((string) token.Value, out n32) || n32 == 0) {
+				if (token.Type != ImapTokenType.Atom || !uint.TryParse ((string) token.Value, out n32)) {
 					Debug.WriteLine ("Expected nz-number as first argument of the 'COPYUID' RESP-CODE, but got: {0}", token);
 					throw UnexpectedToken (token, false);
 				}
