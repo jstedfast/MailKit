ae4d3e695c20f84067b3d37c0d0b5e2e3dadd3c9
Jeffrey Stedfast <fejj@gnome.org>
2015-12-04 23:28:31 -0500

    Implemented HMACMD5 for dnxcore50

--- MailKit/Security/Ntlm/HMACMD5.cs
+++ MailKit/Security/Ntlm/HMACMD5.cs
@@ -26,11 +26,22 @@
 
 using System;
 using System.IO;
+
+#if NETFX_CORE
 using Windows.Security.Cryptography;
 using Windows.Security.Cryptography.Core;
+#else
+using System.Security.Cryptography;
+
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Macs;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.Parameters;
+#endif
 
 namespace MailKit.Security.Ntlm
 {
+#if NETFX_CORE
 	class HMACMD5 : IDisposable
 	{
 		MacAlgorithmProvider algorithm;
@@ -206,4 +217,177 @@ namespace MailKit.Security.Ntlm
 			disposed = true;
 		}
 	}
+#else
+	class HMACMD5 : IDisposable
+	{
+		readonly HMac hash = new HMac (new MD5Digest ());
+		byte[] hashValue, key;
+		bool disposed;
+
+		public HMACMD5 (byte[] key)
+		{
+			Key = key;
+		}
+
+		~HMACMD5 ()
+		{
+			Dispose (false);
+		}
+
+		public byte[] Hash
+		{
+			get {
+				if (hashValue == null)
+					throw new InvalidOperationException ("No hash value computed.");
+
+				return hashValue;
+			}
+		}
+
+		public byte[] Key
+		{
+			get { return key; }
+			set
+			{
+				if (value == null)
+					throw new ArgumentNullException ("value");
+
+				if (key != null)
+					Array.Clear (key, 0, key.Length);
+
+				key = value;
+				Initialize ();
+			}
+		}
+
+		void HashCore (byte[] block, int offset, int size)
+		{
+			hash.BlockUpdate (block, offset, size);
+		}
+
+		byte[] HashFinal ()
+		{
+			var value = new byte[hash.GetMacSize ()];
+
+			hash.DoFinal (value, 0);
+			hash.Reset ();
+
+			return value;
+		}
+
+		public void Initialize ()
+		{
+			hash.Init (new KeyParameter (Key));
+		}
+
+		public void Clear ()
+		{
+			Dispose (false);
+		}
+
+		public byte[] ComputeHash (byte[] buffer, int offset, int count)
+		{
+			if (buffer == null)
+				throw new ArgumentNullException ("buffer");
+
+			if (offset < 0 || offset > buffer.Length)
+				throw new ArgumentOutOfRangeException ("offset");
+
+			if (count < 0 || offset > buffer.Length - count)
+				throw new ArgumentOutOfRangeException ("count");
+
+			if (disposed)
+				throw new ObjectDisposedException ("HashAlgorithm");
+
+			HashCore (buffer, offset, count);
+			hashValue = HashFinal ();
+
+			return hashValue;
+		}
+
+		public byte[] ComputeHash (byte[] buffer)
+		{
+			if (buffer == null)
+				throw new ArgumentNullException ("buffer");
+
+			return ComputeHash (buffer, 0, buffer.Length);
+		}
+
+		public byte[] ComputeHash (Stream inputStream)
+		{
+			// don't read stream unless object is ready to use
+			if (disposed)
+				throw new ObjectDisposedException ("HashAlgorithm");
+
+			var buffer = new byte[4096];
+			int nread;
+
+			do
+			{
+				if ((nread = inputStream.Read (buffer, 0, buffer.Length)) > 0)
+					HashCore (buffer, 0, nread);
+			} while (nread > 0);
+
+			hashValue = HashFinal ();
+
+			return hashValue;
+		}
+
+		public int TransformBlock (byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+		{
+			if (inputBuffer == null)
+				throw new ArgumentNullException ("inputBuffer");
+
+			if (inputOffset < 0 || inputOffset > inputBuffer.Length)
+				throw new ArgumentOutOfRangeException ("inputOffset");
+
+			if (inputCount < 0 || inputOffset > inputBuffer.Length - inputCount)
+				throw new ArgumentOutOfRangeException ("inputCount");
+
+			if (outputBuffer != null) {
+				if (outputOffset < 0 || outputOffset > outputBuffer.Length - inputCount)
+					throw new ArgumentOutOfRangeException ("outputOffset");
+			}
+
+			HashCore (inputBuffer, inputOffset, inputCount);
+
+			if (outputBuffer != null)
+				Buffer.BlockCopy (inputBuffer, inputOffset, outputBuffer, outputOffset, inputCount);
+
+			return inputCount;
+		}
+
+		public byte[] TransformFinalBlock (byte[] inputBuffer, int inputOffset, int inputCount)
+		{
+			if (inputCount < 0)
+				throw new ArgumentOutOfRangeException ("inputCount");
+
+			var outputBuffer = new byte[inputCount];
+
+			// note: other exceptions are handled by Buffer.BlockCopy
+			Buffer.BlockCopy (inputBuffer, inputOffset, outputBuffer, 0, inputCount);
+
+			HashCore (inputBuffer, inputOffset, inputCount);
+			hashValue = HashFinal ();
+
+			return outputBuffer;
+		}
+
+		void Dispose (bool disposing)
+		{
+			if (Key != null)
+			{
+				Array.Clear (Key, 0, Key.Length);
+				Key = null;
+			}
+		}
+
+		public void Dispose ()
+		{
+			Dispose (true);
+			GC.SuppressFinalize (this);
+			disposed = true;
+		}
+	}
+#endif
 }
