cdbf2f725ce716d4905bb7ddea6cc33e971e2e43
Jeffrey Stedfast <jeff@xamarin.com>
2016-01-05 11:12:56 -0500

    Implemented a UniqueIdMap.GetEnumerator()
    This provides an efficient way of getting the full list
    of uid mappings.

--- MailKit/UniqueIdMap.cs
+++ MailKit/UniqueIdMap.cs
@@ -25,16 +25,62 @@
 //
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace MailKit {
+	/// <summary>
+	/// A unique identifier mapping.
+	/// </summary>
+	/// <remarks>
+	/// Maps a source UID to a destination UID.
+	/// </remarks>
+	public class UniqueIdMapping
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MailKit.UniqueIdMapping"/> class.
+		/// </summary>
+		/// <remarks>
+		/// Creates a new <see cref="MailKit.UniqueIdMapping"/>.
+		/// </remarks>
+		/// <param name="src">The source UID.</param>
+		/// <param name="dest">The destination UID.</param>
+		public UniqueIdMapping (UniqueId src, UniqueId dest)
+		{
+			Destination = dest;
+			Source = src;
+		}
+
+		/// <summary>
+		/// Gets the unique identifier of the message in the source folder.
+		/// </summary>
+		/// <remarks>
+		/// Gets the unique identifier of the message in the source folder.
+		/// </remarks>
+		/// <value>The unique identifier of the message in the source folder.</value>
+		public UniqueId Source {
+			get; private set;
+		}
+
+		/// <summary>
+		/// Gets the unique identifier of the message in the destination folder.
+		/// </summary>
+		/// <remarks>
+		/// Gets the unique identifier of the message in the destination folder.
+		/// </remarks>
+		/// <value>The unique identifier of the message in the destination folder.</value>
+		public UniqueId Destination {
+			get; private set;
+		}
+	}
+
 	/// <summary>
 	/// A mapping of unique identifiers.
 	/// </summary>
 	/// <remarks>
 	/// A mapping of unique identifiers.
 	/// </remarks>
-	public class UniqueIdMap
+	public class UniqueIdMap : IEnumerable<UniqueIdMapping>
 	{
 		/// <summary>
 		/// Any empty mapping of unique identifiers.
@@ -43,8 +89,6 @@ namespace MailKit {
 		/// Any empty mapping of unique identifiers.
 		/// </remarks>
 		public static readonly UniqueIdMap Empty = new UniqueIdMap ();
-
-		readonly Dictionary<uint, uint> dictionary = new Dictionary<uint, uint> ();
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="MailKit.UniqueIdMap"/> class.
@@ -69,12 +113,6 @@ namespace MailKit {
 
 			Destination = destination;
 			Source = source;
-
-			var dst = destination.GetEnumerator ();
-			var src = source.GetEnumerator ();
-
-			while (src.MoveNext () && dst.MoveNext ())
-				dictionary[src.Current.Id] = dst.Current.Id;
 		}
 
 		UniqueIdMap ()
@@ -105,18 +143,6 @@ namespace MailKit {
 		}
 
 		/// <summary>
-		/// Checks if the unique identifier of a message has been remapped.
-		/// </summary>
-		/// <remarks>
-		/// Checks if the unique identifier of a message has been remapped.
-		/// </remarks>
-		/// <param name="src">The unique identifier of a message in the source folder.</param>
-		public bool Contains (UniqueId src)
-		{
-			return dictionary.ContainsKey (src.Id);
-		}
-
-		/// <summary>
 		/// Tries to get the remapped unique identifier.
 		/// </summary>
 		/// <remarks>
@@ -127,19 +153,16 @@ namespace MailKit {
 		/// <param name="dest">The unique identifier of the message in the destination folder.</param>
 		public bool TryGetValue (UniqueId src, out UniqueId dest)
 		{
-			uint validity;
-			bool result;
-			uint uid;
+			int index = Source.IndexOf (src);
 
-			if ((result = dictionary.TryGetValue (src.Id, out uid))) {
-				validity = Destination[0].Validity;
-			} else {
-				validity = 0;
+			if (index == -1 || index >= Destination.Count) {
+				dest = UniqueId.Invalid;
+				return false;
 			}
 
-			dest = new UniqueId (validity, uid);
+			dest = Destination[index];
 
-			return result;
+			return true;
 		}
 
 		/// <summary>
@@ -161,6 +184,36 @@ namespace MailKit {
 
 				return uid;
 			}
+		}
+
+		/// <summary>
+		/// Gets the enumerator.
+		/// </summary>
+		/// <remarks>
+		/// Gets the enumerator.
+		/// </remarks>
+		/// <returns>The enumerator.</returns>
+		public IEnumerator<UniqueIdMapping> GetEnumerator ()
+		{
+			var dst = Destination.GetEnumerator ();
+			var src = Source.GetEnumerator ();
+
+			while (src.MoveNext () && dst.MoveNext ())
+				yield return new UniqueIdMapping (src.Current, dst.Current);
+
+			yield break;
+		}
+
+		/// <summary>
+		/// Gets the enumerator.
+		/// </summary>
+		/// <remarks>
+		/// Gets the enumerator.
+		/// </remarks>
+		/// <returns>The enumerator.</returns>
+		IEnumerator IEnumerable.GetEnumerator ()
+		{
+			return GetEnumerator ();
 		}
 	}
 }
