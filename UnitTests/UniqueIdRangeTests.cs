3b37b73f1bead2d63672a4d78b19a1f5797f565c
Jeffrey Stedfast <jeff@xamarin.com>
2016-01-17 10:38:29 -0500

    Added more UniqueIdRange tests

--- UnitTests/UniqueIdRangeTests.cs
+++ UnitTests/UniqueIdRangeTests.cs
@@ -35,13 +35,55 @@ namespace UnitTests {
 	public class UniqueIdRangeTests
 	{
 		[Test]
-		public void TestEnumerator ()
+		public void TestAscending ()
 		{
-			const string example = "20:1";
+			const string example = "1:20";
+			var copy = new UniqueId[20];
 			UniqueIdRange uids;
 
 			Assert.IsTrue (UniqueIdRange.TryParse (example, 20160117, out uids), "Failed to parse uids.");
 			Assert.AreEqual (20160117, uids.Validity, "Validity");
+			Assert.IsTrue (uids.IsReadOnly, "IsReadOnly");
+			Assert.AreEqual (1, uids.Start.Id, "Start");
+			Assert.AreEqual (20, uids.End.Id, "End");
+			Assert.AreEqual (1, uids.Min.Id, "Min");
+			Assert.AreEqual (20, uids.Max.Id, "Max");
+			Assert.AreEqual (example, uids.ToString (), "ToString");
+			Assert.AreEqual (20, uids.Count);
+
+			for (int i = 0; i < uids.Count; i++) {
+				Assert.AreEqual (i, uids.IndexOf (uids[i]));
+				Assert.AreEqual (20160117, uids[i].Validity);
+				Assert.AreEqual (i + 1, uids[i].Id);
+			}
+
+			uids.CopyTo (copy, 0);
+
+			for (int i = 0; i < copy.Length; i++) {
+				Assert.AreEqual (20160117, copy[i].Validity);
+				Assert.AreEqual (i + 1, copy[i].Id);
+			}
+
+			var list = new List<UniqueId> ();
+			foreach (var uid in uids) {
+				Assert.AreEqual (20160117, uid.Validity);
+				list.Add (uid);
+			}
+
+			for (int i = 0; i < list.Count; i++)
+				Assert.AreEqual (i + 1, list[i].Id);
+		}
+
+		[Test]
+		public void TestDescending ()
+		{
+			const string example = "20:1";
+			var copy = new UniqueId[20];
+			UniqueIdRange uids;
+
+			Assert.IsTrue (UniqueIdRange.TryParse (example, 20160117, out uids), "Failed to parse uids.");
+			Assert.AreEqual (20160117, uids.Validity, "Validity");
+			Assert.IsTrue (uids.IsReadOnly, "IsReadOnly");
 			Assert.AreEqual (20, uids.Start.Id, "Start");
 			Assert.AreEqual (1, uids.End.Id, "End");
 			Assert.AreEqual (1, uids.Min.Id, "Min");
@@ -49,8 +91,18 @@ namespace UnitTests {
 			Assert.AreEqual (example, uids.ToString (), "ToString");
 			Assert.AreEqual (20, uids.Count);
 
-			for (int i = 0; i < uids.Count; i++)
+			for (int i = 0; i < uids.Count; i++) {
+				Assert.AreEqual (i, uids.IndexOf (uids[i]));
+				Assert.AreEqual (20160117, uids[i].Validity);
 				Assert.AreEqual (20 - i, uids[i].Id);
+			}
+
+			uids.CopyTo (copy, 0);
+
+			for (int i = 0; i < copy.Length; i++) {
+				Assert.AreEqual (20160117, copy[i].Validity);
+				Assert.AreEqual (20 - i, copy[i].Id);
+			}
 
 			var list = new List<UniqueId> ();
 			foreach (var uid in uids) {
@@ -63,32 +115,16 @@ namespace UnitTests {
 		}
 
 		[Test]
-		public void TestCopyTo ()
+		public void TestNotSupported ()
 		{
-			const string example = "1:20";
-			var copy = new UniqueId[20];
-			UniqueIdRange uids;
+			var range = new UniqueIdRange (UniqueId.MinValue, UniqueId.MaxValue);
 
-			Assert.IsTrue (UniqueIdRange.TryParse (example, 20160117, out uids), "Failed to parse uids.");
-			Assert.AreEqual (20160117, uids.Validity, "Validity");
-			Assert.AreEqual (1, uids.Start.Id, "Start");
-			Assert.AreEqual (20, uids.End.Id, "End");
-			Assert.AreEqual (1, uids.Min.Id, "Min");
-			Assert.AreEqual (20, uids.Max.Id, "Max");
-			Assert.AreEqual (example, uids.ToString (), "ToString");
-			Assert.AreEqual (20, uids.Count);
-
-			for (int i = 0; i < uids.Count; i++) {
-				Assert.AreEqual (20160117, uids[i].Validity);
-				Assert.AreEqual (i + 1, uids[i].Id);
-			}
-
-			uids.CopyTo (copy, 0);
-
-			for (int i = 0; i < copy.Length; i++) {
-				Assert.AreEqual (20160117, copy[i].Validity);
-				Assert.AreEqual (i + 1, copy[i].Id);
-			}
+			Assert.Throws<NotSupportedException> (() => range[5] = new UniqueId (5), "set");
+			Assert.Throws<NotSupportedException> (() => range.Insert (0, new UniqueId (5)), "Insert");
+			Assert.Throws<NotSupportedException> (() => range.Remove (new UniqueId (5)), "Remove");
+			Assert.Throws<NotSupportedException> (() => range.RemoveAt (1), "RemoveAt");
+			Assert.Throws<NotSupportedException> (() => range.Add (new UniqueId (5)), "Add");
+			Assert.Throws<NotSupportedException> (() => range.Clear (), "Clear");
 		}
 	}
 }
