2139a2b7457d7c1026c6c5bae00480c8122e853b
Jeffrey Stedfast <jeff@xamarin.com>
2016-05-02 17:20:35 -0400

    Set ComVisible to true

--- MailKit/Properties/AssemblyInfo.cs
+++ MailKit/Properties/AssemblyInfo.cs
@@ -43,7 +43,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible (false)]
+[assembly: ComVisible (true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid ("2fe79b66-d107-45da-9493-175f59c4a53c")]
