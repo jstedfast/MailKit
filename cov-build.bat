ce5c8d3627800bf94ea085dc72c1e8149f961496
Jeffrey Stedfast <fejj@gnome.org>
2017-04-04 08:18:02 -0400

    Added MailKit.Coverity.sln

--- cov-build.bat
+++ cov-build.bat
@@ -1,1 +1,1 @@
-..\cov-analysis\bin\cov-build.exe --dir cov-int "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /t:Rebuild MailKit.Net45.sln
+..\cov-analysis\bin\cov-build.exe --dir cov-int "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /t:Rebuild MailKit.Coverity.sln
