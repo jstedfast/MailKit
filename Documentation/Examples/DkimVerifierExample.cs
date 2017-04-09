3401225dc340872be209069095c3b3354e072846
Jeffrey Stedfast <jestedfa@microsoft.com>
2017-04-09 06:34:16 -0400

    Updated DkimVerifierExample.cs

--- Documentation/Examples/DkimVerifierExample.cs
+++ Documentation/Examples/DkimVerifierExample.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Text;
 using System.Threading;
+using System.Collections.Generic;
 
 using Heijden.DNS;
 
@@ -13,152 +14,172 @@ using MimeKit.Cryptography;
 
 namespace DkimVerifier
 {
-	class DkimPublicKeyLocator : IDkimPublicKeyLocator
-	{
-		readonly Resolver resolver;
+    class DkimPublicKeyLocator : IDkimPublicKeyLocator
+    {
+        readonly Dictionary<string, AsymmetricKeyParameter> cache;
+        readonly Resolver resolver;
 
-		public DkimPublicKeyLocator ()
-		{
-			resolver = new Resolver ("8.8.8.8") {
-				TransportType = TransportType.Udp,
-				UseCache = true,
-				Retries = 3
-			};
-		}
+        public DkimPublicKeyLocator ()
+        {
+            cache = new Dictionary<string, AsymmetricKeyParameter> ();
 
-		AsymmetricKeyParameter DnsLookup (string domain, string selector, CancellationToken cancellationToken)
-		{
-			var query = selector + "._domainkey." + domain;
-			var response = resolver.Query (query, QType.TXT);
-			var builder = new StringBuilder ();
+            resolver = new Resolver ("8.8.8.8") {
+                TransportType = TransportType.Udp,
+                UseCache = true,
+                Retries = 3
+            };
+        }
 
-			foreach (var record in response.RecordsTXT) {
-				foreach (var text in record.TXT)
-					builder.Append (text);
-			}
+        AsymmetricKeyParameter DnsLookup (string domain, string selector, CancellationToken cancellationToken)
+        {
+            var query = selector + "._domainkey." + domain;
+            AsymmetricKeyParameter pubkey;
 
-			var txt = builder.ToString ();
-			string k = null, p = null;
-			int index = 0;
+            // checked if we've already fetched this key
+            if (cache.TryGetValue (query, out pubkey))
+                return pubkey;
 
-			// parse responses like: "k=rsa; p=<base64>"
-			while (index < txt.Length) {
-				while (index < txt.Length && char.IsWhiteSpace (txt[index]))
-					index++;
+            // make a DNS query
+            var response = resolver.Query (query, QType.TXT);
+            var builder = new StringBuilder ();
 
-				if (index == txt.Length)
-					break;
+            // combine the TXT records into 1 string buffer
+            foreach (var record in response.RecordsTXT) {
+                foreach (var text in record.TXT)
+                    builder.Append (text);
+            }
 
-				// find the end of the key
-				int startIndex = index;
-				while (index < txt.Length && txt[index] != '=')
-					index++;
+            var txt = builder.ToString ();
+            string k = null, p = null;
+            int index = 0;
 
-				if (index == txt.Length)
-					break;
+            // parse the response (will look something like: "k=rsa; p=<base64>")
+            while (index < txt.Length) {
+                while (index < txt.Length && char.IsWhiteSpace (txt[index]))
+                    index++;
 
-				var key = txt.Substring (startIndex, index - startIndex);
+                if (index == txt.Length)
+                    break;
 
-				// skip over the '='
-				index++;
+                // find the end of the key
+                int startIndex = index;
+                while (index < txt.Length && txt[index] != '=')
+                    index++;
 
-				// find the end of the value
-				startIndex = index;
-				while (index < txt.Length && txt[index] != ';')
-					index++;
+                if (index == txt.Length)
+                    break;
 
-				var value = txt.Substring (startIndex, index - startIndex);
+                var key = txt.Substring (startIndex, index - startIndex);
 
-				switch (key) {
-				case "k": k = value; break;
-				case "p": p = value; break;
-				}
+                // skip over the '='
+                index++;
 
-				// skip over the ';'
-				index++;
-			}
+                // find the end of the value
+                startIndex = index;
+                while (index < txt.Length && txt[index] != ';')
+                    index++;
 
-			if (k != null && p != null) {
-				var data = "-----BEGIN PUBLIC KEY-----\r\n" + p + "\r\n-----END PUBLIC KEY-----\r\n";
-				var rawData = Encoding.ASCII.GetBytes (data);
+                var value = txt.Substring (startIndex, index - startIndex);
 
-				using (var stream = new MemoryStream (rawData, false)) {
-					using (var reader = new StreamReader (stream)) {
-						var pem = new PemReader (reader);
+                switch (key) {
+                case "k": k = value; break;
+                case "p": p = value; break;
+                }
 
-						var pubkey = pem.ReadObject () as AsymmetricKeyParameter;
+                // skip over the ';'
+                index++;
+            }
 
-						if (pubkey != null)
-							return pubkey;
-					}
-				}
-			}
+            if (k != null && p != null) {
+                var data = "-----BEGIN PUBLIC KEY-----\r\n" + p + "\r\n-----END PUBLIC KEY-----\r\n";
+                var rawData = Encoding.ASCII.GetBytes (data);
 
-			throw new Exception (string.Format ("Failed to look up public key for: {0}", domain));
-		}
+                using (var stream = new MemoryStream (rawData, false)) {
+                    using (var reader = new StreamReader (stream)) {
+                        var pem = new PemReader (reader);
 
-		public AsymmetricKeyParameter LocatePublicKey (string methods, string domain, string selector, CancellationToken cancellationToken = default (CancellationToken))
-		{
-			var methodList = methods.Split (new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
-			for (int i = 0; i < methodList.Length; i++) {
-				if (methodList[i] == "dns/txt")
-					return DnsLookup (domain, selector, cancellationToken);
-			}
+                        pubkey = pem.ReadObject () as AsymmetricKeyParameter;
 
-			throw new NotSupportedException (string.Format ("{0} is not a supported lookup method.", methods));
-		}
-	}
+                        if (pubkey != null) {
+                            cache.Add (query, pubkey);
 
-	class Program
-	{
-		public static void Main (string[] args)
-		{
-			if (args.Length == 0) {
-				Help ();
-				return;
-			}
+                            return pubkey;
+                        }
+                    }
+                }
+            }
 
-			for (int i = 0; i < args.Length; i++) {
-				if (args[i] == "--help") {
-					Help ();
-					return;
-				}
-			}
+            throw new Exception (string.Format ("Failed to look up public key for: {0}", domain));
+        }
 
-			var locator = new DkimPublicKeyLocator ();
+        public AsymmetricKeyParameter LocatePublicKey (string methods, string domain, string selector, CancellationToken cancellationToken = default (CancellationToken))
+        {
+            var methodList = methods.Split (new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < methodList.Length; i++) {
+                if (methodList[i] == "dns/txt")
+                    return DnsLookup (domain, selector, cancellationToken);
+            }
 
-			for (int i = 0; i < args.Length; i++) {
-				if (!File.Exists (args[i])) {
-					Console.Error.WriteLine ("{0}: No such file.", args[i]);
-					continue;
-				}
+            throw new NotSupportedException (string.Format ("{0} does not include any suported lookup methods.", methods));
+        }
+    }
 
-				var message = MimeMessage.Load (args[i]);
-				var index = message.Headers.IndexOf (HeaderId.DkimSignature);
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0) {
+                Help ();
+                return;
+            }
 
-				if (index == -1) {
-					Console.Error.WriteLine ("{0}: No DKIM-Signature header found.", args[i]);
-					continue;
-				}
+            for (int i = 0; i < args.Length; i++) {
+                if (args[i] == "--help") {
+                    Help ();
+                    return;
+                }
+            }
 
-				var dkim = message.Headers[index];
+            var locator = new DkimPublicKeyLocator ();
 
-				if (message.Verify (dkim, locator)) {
-					// the DKIM-Signature header is valid!
-					Console.WriteLine ("{0}: valid DKIM-Signature!", args[i]);
-				} else {
-					// the DKIM-Signature is invalid
-					Console.WriteLine ("{0}: invalid DKIM-Signature!", args[i]);
-				}
-			}
-		}
+            for (int i = 0; i < args.Length; i++) {
+                if (!File.Exists (args[i])) {
+                    Console.Error.WriteLine ("{0}: No such file.", args[i]);
+                    continue;
+                }
 
-		static void Help ()
-		{
-			Console.WriteLine ("Usage is: DkimVerifier [options] [messages]");
-			Console.WriteLine ();
-			Console.WriteLine ("Options:");
-			Console.WriteLine ("  --help               This help menu.");
-		}
-	}
+                Console.Write ("{0} -> ", args[i]);
+
+                var message = MimeMessage.Load (args[i]);
+                var index = message.Headers.IndexOf (HeaderId.DkimSignature);
+
+                if (index == -1) {
+                    Console.WriteLine ("NO SIGNATURE");
+                    continue;
+                }
+
+                var dkim = message.Headers[index];
+
+                if (message.Verify (dkim, locator)) {
+                    // the DKIM-Signature header is valid!
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine ("VALID");
+                    Console.ResetColor ();
+                } else {
+                    // the DKIM-Signature is invalid!
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine ("INVALID");
+                    Console.ResetColor ();
+                }
+            }
+        }
+
+        static void Help ()
+        {
+            Console.WriteLine ("Usage is: DkimVerifier [options] [messages]");
+            Console.WriteLine ();
+            Console.WriteLine ("Options:");
+            Console.WriteLine ("  --help               This help menu.");
+        }
+    }
 }
