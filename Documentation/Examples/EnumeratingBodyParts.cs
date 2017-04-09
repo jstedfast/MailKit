99b3cf7a3d8fb0f681df310fe01d164df9fcdf90
Jeffrey Stedfast <jestedfa@microsoft.com>
2017-04-08 20:17:12 -0400

    Updated docs

--- Documentation/Examples/EnumeratingBodyParts.cs
+++
@@ -1,8 +0,0 @@
-﻿
-foreach (var attachment in message.Attachments) {
-	var fileName = attachment.FileName;
-
-	using (var stream = File.Create (fileName)) {
-		attachment.ContentObject.DecodeTo (stream);
-	}
-}
