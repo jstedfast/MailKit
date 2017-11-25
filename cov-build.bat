f2218c228cf74608bfffd52608a595939924e64a
Jeffrey Stedfast <fejj@gnome.org>
2017-11-25 12:00:02 -0500

    Updated cov-build.bat

--- cov-build.bat
+++ cov-build.bat
@@ -1,1 +1,1 @@
-..\cov-analysis\bin\cov-build.exe --dir cov-int "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /t:Rebuild MailKit.Coverity.sln
+..\cov-analysis\bin\cov-build.exe --dir cov-int "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /t:Rebuild MailKit.Coverity.sln
