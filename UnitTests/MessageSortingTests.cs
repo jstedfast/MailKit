9f8461946a06686db7ba4cea1568eb842d007a91
Jeffrey Stedfast <jestedfa@microsoft.com>
2018-10-22 22:37:03 -0400

    Added more MessageSorter unit tests

--- UnitTests/MessageSortingTests.cs
+++ UnitTests/MessageSortingTests.cs
@@ -56,44 +56,54 @@ namespace UnitTests {
 			Assert.Throws<ArgumentException> (() => MessageSorter.Sort ((IEnumerable<MessageSummary>) messages, orderBy));
 		}
 
-		[Test]
-		public void TestSorting ()
+		static List<MessageSummary> Create ()
 		{
 			var messages = new List<MessageSummary> ();
 			MessageSummary summary;
 
 			summary = new MessageSummary (0);
-			summary.Fields = MessageSummaryItems.Envelope | MessageSummaryItems.Size;
+			summary.Fields = MessageSummaryItems.Envelope | MessageSummaryItems.Size | MessageSummaryItems.ModSeq;
 			summary.Envelope = new Envelope ();
 			summary.Envelope.Date = DateTimeOffset.Now.AddSeconds (-2);
 			summary.Envelope.Subject = "aaaa";
 			summary.Envelope.From.Add (new MailboxAddress ("A", "a@a.com"));
 			summary.Envelope.To.Add (new MailboxAddress ("A", "a@a.com"));
 			summary.Envelope.Cc.Add (new MailboxAddress ("A", "a@a.com"));
+			summary.ModSeq = 80290;
 			summary.Size = 520;
 			messages.Add (summary);
 
 			summary = new MessageSummary (1);
-			summary.Fields = MessageSummaryItems.Envelope | MessageSummaryItems.Size;
+			summary.Fields = MessageSummaryItems.Envelope | MessageSummaryItems.Size | MessageSummaryItems.ModSeq;
 			summary.Envelope = new Envelope ();
 			summary.Envelope.Date = DateTimeOffset.Now.AddSeconds (-1);
 			summary.Envelope.Subject = "bbbb";
 			summary.Envelope.From.Add (new MailboxAddress ("B", "b@b.com"));
 			summary.Envelope.To.Add (new MailboxAddress ("B", "b@b.com"));
 			summary.Envelope.Cc.Add (new MailboxAddress ("B", "b@b.com"));
+			summary.ModSeq = 70642;
 			summary.Size = 265;
 			messages.Add (summary);
 
 			summary = new MessageSummary (2);
-			summary.Fields = MessageSummaryItems.Envelope | MessageSummaryItems.Size;
+			summary.Fields = MessageSummaryItems.Envelope | MessageSummaryItems.Size | MessageSummaryItems.ModSeq;
 			summary.Envelope = new Envelope ();
 			summary.Envelope.Date = DateTimeOffset.Now;
 			summary.Envelope.Subject = "cccc";
 			summary.Envelope.From.Add (new MailboxAddress ("C", "c@c.com"));
 			summary.Envelope.To.Add (new MailboxAddress ("C", "c@c.com"));
 			summary.Envelope.Cc.Add (new MailboxAddress ("C", "c@c.com"));
+			summary.ModSeq = 80290;
 			summary.Size = 520;
 			messages.Add (summary);
+
+			return messages;
+		}
+
+		[Test]
+		public void TestSorting ()
+		{
+			var messages = Create ();
 
 			messages.Sort (new[] { OrderBy.Arrival });
 			Assert.AreEqual (0, messages[0].Index, "Sorting by arrival failed.");
@@ -159,6 +169,89 @@ namespace UnitTests {
 			Assert.AreEqual (0, messages[0].Index, "Sorting by cc failed.");
 			Assert.AreEqual (1, messages[1].Index, "Sorting by cc failed.");
 			Assert.AreEqual (2, messages[2].Index, "Sorting by cc failed.");
+
+			messages.Sort (new [] { new OrderBy (OrderByType.ModSeq, SortOrder.Ascending), OrderBy.Arrival });
+			Assert.AreEqual (1, messages[0].Index, "Sorting by modseq failed.");
+			Assert.AreEqual (0, messages[1].Index, "Sorting by modseq failed.");
+			Assert.AreEqual (2, messages[2].Index, "Sorting by modseq failed.");
+		}
+
+		[Test]
+		public void TestSortingEnumerable ()
+		{
+			var messages = Create ();
+			IEnumerable<MessageSummary> enumerable = messages;
+			IList<MessageSummary> sorted;
+
+			sorted = enumerable.Sort (new [] { OrderBy.Arrival });
+			Assert.AreEqual (0, sorted[0].Index, "Sorting by arrival failed.");
+			Assert.AreEqual (1, sorted[1].Index, "Sorting by arrival failed.");
+			Assert.AreEqual (2, sorted[2].Index, "Sorting by arrival failed.");
+
+			sorted = enumerable.Sort (new [] { OrderBy.ReverseArrival });
+			Assert.AreEqual (2, sorted[0].Index, "Sorting by reverse arrival failed.");
+			Assert.AreEqual (1, sorted[1].Index, "Sorting by reverse arrival failed.");
+			Assert.AreEqual (0, sorted[2].Index, "Sorting by reverse arrival failed.");
+
+			sorted = enumerable.Sort (new [] { OrderBy.Subject });
+			Assert.AreEqual (0, sorted[0].Index, "Sorting by subject failed.");
+			Assert.AreEqual (1, sorted[1].Index, "Sorting by subject failed.");
+			Assert.AreEqual (2, sorted[2].Index, "Sorting by subject failed.");
+
+			sorted = enumerable.Sort (new [] { OrderBy.ReverseSubject });
+			Assert.AreEqual (2, sorted[0].Index, "Sorting by reverse subject failed.");
+			Assert.AreEqual (1, sorted[1].Index, "Sorting by reverse subject failed.");
+			Assert.AreEqual (0, sorted[2].Index, "Sorting by reverse subject failed.");
+
+			sorted = enumerable.Sort (new [] { OrderBy.Size, OrderBy.Arrival });
+			Assert.AreEqual (1, sorted[0].Index, "Sorting by size failed.");
+			Assert.AreEqual (0, sorted[1].Index, "Sorting by size failed.");
+			Assert.AreEqual (2, sorted[2].Index, "Sorting by size failed.");
+
+			sorted = enumerable.Sort (new [] { OrderBy.Date });
+			Assert.AreEqual (0, sorted[0].Index, "Sorting by date failed.");
+			Assert.AreEqual (1, sorted[1].Index, "Sorting by date failed.");
+			Assert.AreEqual (2, sorted[2].Index, "Sorting by date failed.");
+
+			sorted = enumerable.Sort (new [] { OrderBy.Size, OrderBy.Subject });
+			Assert.AreEqual (1, sorted[0].Index, "Sorting by size+subject failed.");
+			Assert.AreEqual (0, sorted[1].Index, "Sorting by size+subject failed.");
+			Assert.AreEqual (2, sorted[2].Index, "Sorting by size+subject failed.");
+
+			sorted = enumerable.Sort (new [] { OrderBy.ReverseSize, OrderBy.ReverseSubject });
+			Assert.AreEqual (2, sorted[0].Index, "Sorting by reversed size+subject failed.");
+			Assert.AreEqual (0, sorted[1].Index, "Sorting by reversed size+subject failed.");
+			Assert.AreEqual (1, sorted[2].Index, "Sorting by reversed size+subject failed.");
+
+			sorted = enumerable.Sort (new [] { OrderBy.DisplayFrom });
+			Assert.AreEqual (0, sorted[0].Index, "Sorting by display-from failed.");
+			Assert.AreEqual (1, sorted[1].Index, "Sorting by display-from failed.");
+			Assert.AreEqual (2, sorted[2].Index, "Sorting by display-from failed.");
+
+			sorted = enumerable.Sort (new [] { OrderBy.From });
+			Assert.AreEqual (0, sorted[0].Index, "Sorting by from failed.");
+			Assert.AreEqual (1, sorted[1].Index, "Sorting by from failed.");
+			Assert.AreEqual (2, sorted[2].Index, "Sorting by from failed.");
+
+			sorted = enumerable.Sort (new [] { OrderBy.DisplayTo });
+			Assert.AreEqual (0, sorted[0].Index, "Sorting by display-to failed.");
+			Assert.AreEqual (1, sorted[1].Index, "Sorting by display-to failed.");
+			Assert.AreEqual (2, sorted[2].Index, "Sorting by display-to failed.");
+
+			sorted = enumerable.Sort (new [] { OrderBy.To });
+			Assert.AreEqual (0, sorted[0].Index, "Sorting by to failed.");
+			Assert.AreEqual (1, sorted[1].Index, "Sorting by to failed.");
+			Assert.AreEqual (2, sorted[2].Index, "Sorting by to failed.");
+
+			sorted = enumerable.Sort (new [] { OrderBy.Cc });
+			Assert.AreEqual (0, sorted[0].Index, "Sorting by cc failed.");
+			Assert.AreEqual (1, sorted[1].Index, "Sorting by cc failed.");
+			Assert.AreEqual (2, sorted[2].Index, "Sorting by cc failed.");
+
+			sorted = enumerable.Sort (new [] { new OrderBy (OrderByType.ModSeq, SortOrder.Ascending), OrderBy.Arrival });
+			Assert.AreEqual (1, sorted[0].Index, "Sorting by modseq failed.");
+			Assert.AreEqual (0, sorted[1].Index, "Sorting by modseq failed.");
+			Assert.AreEqual (2, sorted[2].Index, "Sorting by modseq failed.");
 		}
 	}
 }
