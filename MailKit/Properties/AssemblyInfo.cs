ad501a3bef1122532e635efca45de8059faebc9e
Jeffrey Stedfast <fejj@gnome.org>
2018-12-03 17:34:05 -0500

    bumped version to 2.1.0.2

--- MailKit/Properties/AssemblyInfo.cs
+++ MailKit/Properties/AssemblyInfo.cs
@@ -79,6 +79,6 @@ using System.Runtime.InteropServices;
 //
 // If there have only been bug fixes, bump the Micro Version and/or the Build Number
 //    in the AssemblyFileVersion attribute.
-[assembly: AssemblyInformationalVersion ("2.1.0.0")]
-[assembly: AssemblyFileVersion ("2.1.0.0")]
+[assembly: AssemblyInformationalVersion ("2.1.0.2")]
+[assembly: AssemblyFileVersion ("2.1.0.2")]
 [assembly: AssemblyVersion ("2.1.0.0")]
