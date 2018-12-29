632057c010543e10e6c1b08e8ea314db7b0d754b
Jeffrey Stedfast <jestedfa@microsoft.com>
2018-12-29 11:34:57 -0500

    Drastically simplify ConnectAsync logic thanks to a suggestion by @SirRufo

--- MailKit/Net/SocketUtils.cs
+++ MailKit/Net/SocketUtils.cs
@@ -36,91 +36,6 @@ namespace MailKit.Net
 	static class SocketUtils
 	{
 #if NETSTANDARD_2_0 || NET_4_5 || __MOBILE__
-		class AsyncConnectState
-		{
-			public readonly CancellationToken CancellationToken;
-			public readonly Socket Socket;
-			public readonly string Host;
-			public readonly int Port;
-			public bool IsConnected;
-			Exception ex;
-
-			public AsyncConnectState (Socket socket, string host, int port, CancellationToken cancellationToken)
-			{
-				CancellationToken = cancellationToken;
-				Socket = socket;
-				Host = host;
-				Port = port;
-			}
-
-			public void SetCanceled ()
-			{
-				ex = ex ?? new OperationCanceledException ();
-			}
-
-			public void SetException (Exception ex)
-			{
-				this.ex = this.ex ?? ex;
-			}
-
-			public void Throw ()
-			{
-				if (ex != null)
-					throw ex;
-			}
-		}
-
-		// Note: EndConnect needs to catch all exceptions
-		static void EndConnect (IAsyncResult ar)
-		{
-			var connection = (AsyncConnectState) ar.AsyncState;
-
-			if (connection.CancellationToken.IsCancellationRequested) {
-				try {
-					connection.Socket.Close ();
-				} catch (SocketException) {
-				}
-				connection.SetCanceled ();
-				return;
-			}
-
-			try {
-				connection.Socket.EndConnect (ar);
-				connection.IsConnected = true;
-			} catch (Exception ex) {
-				connection.SetException (ex);
-			}
-		}
-
-		static void Connect (Socket socket, string host, int port, AsyncConnectState connection, CancellationToken cancellationToken)
-		{
-			var ar = socket.BeginConnect (host, port, EndConnect, connection);
-			var waitHandles = new WaitHandle[] { ar.AsyncWaitHandle, cancellationToken.WaitHandle };
-
-			WaitHandle.WaitAny (waitHandles);
-
-			try {
-				cancellationToken.ThrowIfCancellationRequested ();
-			} catch (OperationCanceledException) {
-				socket.Close ();
-				throw;
-			}
-
-			connection.Throw ();
-
-			if (!socket.Connected) {
-				// MONO BUG: If the AddressFamily is not supported (e.g. IPv6), then we could get into this situation...
-				throw new SocketException ((int) SocketError.AddressFamilyNotSupported);
-			}
-		}
-
-		static void ConnectAsync (object state)
-		{
-			var connection = (AsyncConnectState) state;
-
-			Connect (connection.Socket, connection.Host, connection.Port, connection, connection.CancellationToken);
-		}
-
 		public static async Task<Socket> ConnectAsync (string host, int port, IPEndPoint localEndPoint, bool doAsync, CancellationToken cancellationToken)
 		{
 			cancellationToken.ThrowIfCancellationRequested ();
@@ -132,12 +47,22 @@ namespace MailKit.Net
 					socket.Bind (localEndPoint);
 
 				if (doAsync || cancellationToken.CanBeCanceled) {
-					var connection = new AsyncConnectState (socket, host, port, cancellationToken);
+					var tcs = new TaskCompletionSource<bool> ();
 
-					if (doAsync) {
-						await Task.Factory.StartNew (ConnectAsync, connection, cancellationToken, TaskCreationOptions.LongRunning, TaskScheduler.Default).ConfigureAwait (false);
-					} else {
-						Connect (socket, host, port, connection, cancellationToken);
+					using (var registration = cancellationToken.Register (() => tcs.SetCanceled (), false)) {
+						var ar = socket.BeginConnect (host, port, e => tcs.SetResult (true), null);
+
+						try {
+							if (doAsync)
+								await tcs.Task.ConfigureAwait (false);
+							else
+								tcs.Task.Wait ();
+
+							socket.EndConnect (ar);
+						} catch (Exception) {
+							socket.Close ();
+							throw;
+						}
 					}
 				} else {
 					socket.Connect (host, port);
