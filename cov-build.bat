4b939064a4b6ac2cd8d81247fcb1abbcca2be3b4
Jeffrey Stedfast <jestedfa@microsoft.com>
2020-05-25 10:14:58 -0400

    Update cov-build.bat

--- cov-build.bat
+++ cov-build.bat
@@ -1,1 +1,1 @@
-..\cov-analysis\bin\cov-build.exe --dir cov-int "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /t:Rebuild MailKit.Coverity.sln
+..\cov-analysis\bin\cov-build.exe --dir cov-int "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" /t:Rebuild MailKit.Coverity.sln
