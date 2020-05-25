93fe3427ff86d18e7b540acbc6d166426040d08a
Jeffrey Stedfast <jestedfa@microsoft.com>
2020-05-25 10:16:51 -0400

    Updated samples to use PackageReference

--- samples/ImapIdle/ImapIdle/Properties/AssemblyInfo.cs
+++ samples/ImapIdle/ImapIdle/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@
 //
 // Author: Jeffrey Stedfast <jeff@xamarin.com>
 //
-// Copyright (c) 2014 Xamarin Inc. (www.xamarin.com)
+// Copyright (c) 2014-2020 Xamarin Inc. (www.xamarin.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
