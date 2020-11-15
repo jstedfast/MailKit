11de20c695d82c433309d44d2dee7b17d61c799e
Jeffrey Stedfast <jestedfa@microsoft.com>
2020-11-14 20:08:01 -0500

    Removed release.bat

--- release.bat
+++
@@ -1,2 +0,0 @@
-"%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /p:Configuration=Release /p:Platform="Any CPU" /t:Rebuild MailKit.Win.sln
-"%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /p:Configuration=Release /p:Platform="Any CPU" /t:Rebuild MailKit.sln
