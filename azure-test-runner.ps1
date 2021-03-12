925bdda11d0669e139dcc55e3f338b5265d036e5
Jeffrey Stedfast <jestedfa@microsoft.com>
2021-03-12 16:50:58 -0500

    Removed azure-pipelines.yml and azure-test-runner.ps1

--- azure-test-runner.ps1
+++
@@ -1,3 +0,0 @@
-$NUnitConsoleRunner = Join-Path $Home ".nuget\packages\nunit.consolerunner\3.12.0\tools\nunit3-console.exe"
-
-& $NUnitConsoleRunner --domain:single "UnitTests\bin\Release\net48\UnitTests.dll"
