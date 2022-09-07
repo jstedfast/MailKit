a7b2c590d068d4e709fe7ebac5d433010138dd51
Jeffrey Stedfast <jestedfa@microsoft.com>
2022-09-07 17:33:05 -0400

    Updated coveralls.ps1 to use the dotnet tool version of coveralls.net

--- coveralls.ps1
+++ coveralls.ps1
@@ -1,28 +1,49 @@
 [xml]$project = Get-Content UnitTests\UnitTests.csproj
+
+$nugetDir = Join-Path $Home ".nuget"
+$nugetPackagesDir = Join-Path $nugetDir "packages"
+
+# Get the NUnit.ConsoleRunner executable path
 $packageReference = $project.SelectSingleNode("/Project/ItemGroup/PackageReference[@Include='NUnit.ConsoleRunner']")
 $consoleRunnerVersion = $packageReference.GetAttribute("Version")
-$consoleRunnerBasePackageDir = Join-Path $Home ".nuget\packages\nunit.consolerunner"
+$consoleRunnerBasePackageDir = Join-Path $nugetPackagesDir "nunit.consolerunner"
 $consoleRunnerPackageDir = Join-Path $consoleRunnerBasePackageDir $consoleRunnerVersion
+$consoleRunnerToolsDir = Join-Path $consoleRunnerPackageDir "tools"
 
-$NUnitConsoleRunner = Join-Path $consoleRunnerPackageDir "tools\nunit3-console.exe"
-$Coveralls = Join-Path $Home ".nuget\packages\coveralls.net\0.7.0\tools\csmacnz.Coveralls.exe"
-$OpenCoverDir = Join-Path $Home ".nuget\packages\opencover\4.6.519\tools"
-$OpenCoverProfiler32 = Join-Path $OpenCoverDir "x86\OpenCover.Profiler.dll"
-$OpenCoverProfiler64 = Join-Path $OpenCoverDir "x64\OpenCover.Profiler.dll"
-$OpenCover = Join-Path $OpenCoverDir "OpenCover.Console.exe"
-$OutputDir = "UnitTests\bin\Debug\net48"
+$NUnitConsoleRunner = Join-Path $consoleRunnerToolsDir "nunit3-console.exe"
+
+# Get the OpenCover executable path
+$packageReference = $project.SelectSingleNode("/Project/ItemGroup/PackageReference[@Include='OpenCover']")
+$openCoverVersion = $packageReference.GetAttribute("Version")
+$openCoverBasePackageDir = Join-Path $nugetPackagesDir "opencover"
+$openCoverPackageDir = Join-Path $openCoverBasePackageDir $openCoverVersion
+$openCoverToolsDir = Join-Path $openCoverPackageDir "tools"
+
+$OpenCoverProfiler32 = Join-Path $openCoverToolsDir "x86\OpenCover.Profiler.dll"
+$OpenCoverProfiler64 = Join-Path $openCoverToolsDir "x64\OpenCover.Profiler.dll"
+$OpenCover = Join-Path $openCoverToolsDir "OpenCover.Console.exe"
+
+# Get the OutputPath
+$targetFramework = $project.SelectSingleNode("/Project/PropertyGroup/TargetFramework")
+$OutputDir = Join-Path "UnitTests\bin\Debug" $targetFramework.InnerText
 
 & regsvr32 $OpenCoverProfiler32
 
 & regsvr32 $OpenCoverProfiler64
 
+Write-Host "Running the UnitTests"
+
+# Run OpenCover
 & $OpenCover -filter:"+[MailKit]* -[MimeKit]* -[UnitTests]*" `
 	-target:"$NUnitConsoleRunner" `
 	-targetdir:"$OutputDir" `
 	-targetargs:"--domain:single UnitTests.dll" `
 	-output:opencover.xml
 
-& $Coveralls --opencover -i opencover.xml `
+Write-Host "Uploading coverage data to coveralls.io"
+
+# Upload code-coverage data to coveralls.io
+& dotnet tool run csmacnz.Coveralls --opencover -i opencover.xml `
 	--repoToken $env:COVERALLS_REPO_TOKEN `
 	--useRelativePaths `
 	--basePath $OutputDir `
