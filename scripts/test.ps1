bea9a9edbe79ae3778f1fa1241a75ed894872bb1
Jeffrey Stedfast <jestedfa@microsoft.com>
2022-09-08 11:15:18 -0400

    Updated test.ps1

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -2,44 +2,38 @@
 param (
     [Parameter()]
     [string]
-    $TestResults = "TestResults.xml",
+    $Configuration = "Debug",
     [string]
     $GenerateCodeCoverage = "no"
 )
 
-Write-Output "TestResults:          $TestResults"
+Write-Output "Configuration:        $Configuration"
 Write-Output "GenerateCodeCoverage: $GenerateCodeCoverage"
 Write-Output ""
 
 [xml]$project = Get-Content UnitTests\UnitTests.csproj
 
-$nugetDir = Join-Path $Home ".nuget"
-$nugetPackagesDir = Join-Path $nugetDir "packages"
+$nugetPackagesDir = Join-Path $Home ".nuget" "packages"
 
 # Get the NUnit.ConsoleRunner executable path
 $packageReference = $project.SelectSingleNode("/Project/ItemGroup/PackageReference[@Include='NUnit.ConsoleRunner']")
 $consoleRunnerVersion = $packageReference.GetAttribute("Version")
-$consoleRunnerBasePackageDir = Join-Path $nugetPackagesDir "nunit.consolerunner"
-$consoleRunnerPackageDir = Join-Path $consoleRunnerBasePackageDir $consoleRunnerVersion
-$consoleRunnerToolsDir = Join-Path $consoleRunnerPackageDir "tools"
 
-$NUnitConsoleRunner = Join-Path $consoleRunnerToolsDir "nunit3-console.exe"
+$NUnitConsoleRunner = Join-Path $nugetPackagesDir "nunit.consolerunner" $consoleRunnerVersion "tools" "nunit3-console.exe"
 
 # Get the OutputPath
 $targetFramework = $project.SelectSingleNode("/Project/PropertyGroup/TargetFramework")
-$OutputDir = Join-Path "UnitTests\bin\Debug" $targetFramework.InnerText
+$OutputDir = Join-Path "UnitTests" "bin" $Configuration $targetFramework.InnerText
 $UnitTestsAssembly = Join-Path $OutputDir "UnitTests.dll"
 
 if ($GenerateCodeCoverage -eq 'yes') {
     # Get the OpenCover executable path
     $packageReference = $project.SelectSingleNode("/Project/ItemGroup/PackageReference[@Include='OpenCover']")
     $openCoverVersion = $packageReference.GetAttribute("Version")
-    $openCoverBasePackageDir = Join-Path $nugetPackagesDir "opencover"
-    $openCoverPackageDir = Join-Path $openCoverBasePackageDir $openCoverVersion
-    $openCoverToolsDir = Join-Path $openCoverPackageDir "tools"
+    $openCoverToolsDir = Join-Path $nugetPackagesDir "opencover" $openCoverVersion "tools"
 
-    $OpenCoverProfiler32 = Join-Path $openCoverToolsDir "x86\OpenCover.Profiler.dll"
-    $OpenCoverProfiler64 = Join-Path $openCoverToolsDir "x64\OpenCover.Profiler.dll"
+    $OpenCoverProfiler32 = Join-Path $openCoverToolsDir "x86" "OpenCover.Profiler.dll"
+    $OpenCoverProfiler64 = Join-Path $openCoverToolsDir "x64" "OpenCover.Profiler.dll"
     $OpenCover = Join-Path $openCoverToolsDir "OpenCover.Console.exe"
 
     try {
@@ -59,11 +53,11 @@ if ($GenerateCodeCoverage -eq 'yes') {
     # Run OpenCover
     & $OpenCover -filter:"+[MailKit]* -[MimeKit]* -[UnitTests]*" `
         -target:"$NUnitConsoleRunner" `
-        -targetargs:"--domain:single --output:$TestResults $UnitTestsAssembly" `
+        -targetargs:"--domain:single $UnitTestsAssembly" `
         -output:opencover.xml
 } else {
     Write-Output "Running the UnitTests"
 
     # Run OpenCover
-    & $NUnitConsoleRunner --domain:single --output:$TestResults $UnitTestsAssembly
+    & $NUnitConsoleRunner --domain:single $UnitTestsAssembly
 }
