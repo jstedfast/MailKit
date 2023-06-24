b3ec2cdec631b622807fb39312ed3e01477b2a79
Jeffrey Stedfast <jestedfa@microsoft.com>
2023-06-24 11:30:18 -0400

    Bump version to 4.1.0.1

--- MailKit/Properties/AssemblyInfo.cs
+++ MailKit/Properties/AssemblyInfo.cs
@@ -79,6 +79,6 @@ using System.Runtime.InteropServices;
 //
 // If there have only been bug fixes, bump the Micro Version and/or the Build Number
 //    in the AssemblyFileVersion attribute.
-[assembly: AssemblyInformationalVersion ("4.1.0.0")]
-[assembly: AssemblyFileVersion ("4.1.0.0")]
+[assembly: AssemblyInformationalVersion ("4.1.0.1")]
+[assembly: AssemblyFileVersion ("4.1.0.1")]
 [assembly: AssemblyVersion ("4.1.0.0")]
