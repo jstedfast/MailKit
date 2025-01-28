449d58aac60ac9d445f194c3fcf51bfd8b577a34
Jeffrey Stedfast <jestedfa@microsoft.com>
2025-01-28 15:21:07 -0500

    Updated copyright notices

--- MailKit/Properties/AssemblyInfo.cs
+++ MailKit/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@
 //
 // Author: Jeffrey Stedfast <jestedfa@microsoft.com>
 //
-// Copyright (c) 2013-2024 .NET Foundation and Contributors
+// Copyright (c) 2013-2025 .NET Foundation and Contributors
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -36,7 +36,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany (".NET Foundation")]
 [assembly: AssemblyProduct ("MailKit")]
-[assembly: AssemblyCopyright ("Copyright © 2013-2024 .NET Foundation and Contributors")]
+[assembly: AssemblyCopyright ("Copyright © 2013-2025 .NET Foundation and Contributors")]
 [assembly: AssemblyTrademark (".NET Foundation")]
 [assembly: AssemblyCulture ("")]
 
