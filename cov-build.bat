e5f891d7b03dd866509ed151fcf71f88b7fc8d15
Jeffrey Stedfast <jestedfa@microsoft.com>
2025-04-27 15:00:48 -0400

    Remove old cov-int build data when generating new coverage

--- cov-build.bat
+++ cov-build.bat
@@ -1,1 +1,3 @@
+rmdir /q /s cov-int
+del cov-int.zip
 ..\cov-analysis\bin\cov-build.exe --dir cov-int "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe" /t:Rebuild MailKit.Coverity.sln
