74994ac14f541c3c558fee9b1c6e62fbf069952e
Jeffrey Stedfast <jestedfa@microsoft.com>
2025-09-27 09:48:28 -0400

    Allow the coveralls.net dotnet tool to use a newer .NET runtime

--- scripts/coveralls.ps1
+++ scripts/coveralls.ps1
@@ -7,7 +7,7 @@ $debugDir = Join-Path $binDir "Debug"
 $OutputDir = Join-Path $debugDir $targetFramework.InnerText
 
 # Upload code-coverage data to coveralls.io
-& dotnet tool run csmacnz.Coveralls --opencover -i coverage.xml `
+& dotnet tool run --allow-roll-forward csmacnz.Coveralls --opencover -i coverage.xml `
 	--repoToken $env:COVERALLS_REPO_TOKEN `
 	--useRelativePaths `
 	--basePath $OutputDir `
